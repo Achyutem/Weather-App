eae5f23914ce0f1b2db9df20e0966330e3295a75
achyutem <achyutem.ad@gmail.com>
2024-06-15 13:47:10 +0530

    gradient and api

--- README.md
+++ README.md
@@ -5,12 +5,11 @@
 
 **Here are few samples -**
 
-![sample](https://github.com/Achyutem/Weather-App/blob/main/sample.jpg)
-![sample2](https://github.com/Achyutem/Weather-App/blob/main/Sample2.png)
-![sample3](https://github.com/Achyutem/Weather-App/blob/main/Sample3.png)
+![sample](https://github.com/Achyutem/Weather-App/blob/main/Sample1.jpg)
+![sample2](https://github.com/Achyutem/Weather-App/blob/main/Sample3.png)
 
 ## TODO LIST
 
-- [ ] Add Gradient to the card.
+- [x] Add Gradient to the card.
 - [ ] Build a node backend to privatise apikey.
-- [ ] Use Unsplash API to fetch image.
+- [x] Use Unsplash API to fetch image.
--- script.js
+++ script.js
@@ -1,44 +1,92 @@
-let weather = {
-    apiKey: "insert you api key here",
-    fetchWeather: function (city) {
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q="
-            + city
-            + "&units=metric&appid="
-            + this.apiKey
-        )
-            .then((response) => response.json())
-            .then((data) => this.displayWeather(data));
-    },
-    displayWeather: function (data) {
-        const { name } = data;
-        const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
-        const { speed } = data.wind;
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°";
-        document.querySelector(".humidity").innerText = "Humidity is: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
-        document.querySelector(".weather").classList.remove('loading')
-        document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
-    },
-    search: function () {
-        this.fetchWeather(document.querySelector(".search-bar").value)
-    },
-};
-
-document.querySelector(".search button")
-    .addEventListener('click', function () {
-        weather.search();
-    });
-
-document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-    if (event.key == 'Enter') {
-        weather.search()
-    }
-})
-
+let weather = {
+    apiKey: "your weather api key",
+    unsplashKey: 'unsplash api key',
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove('loading');
+
+        this.fetchUnsplashImage(name);
+    },
+    fetchUnsplashImage: function (city) {
+        fetch(
+            "https://api.unsplash.com/search/photos?page=1&query="
+            + city
+            + "&client_id="
+            + this.unsplashKey
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.results && data.results.length > 0) {
+                    const random = Math.floor(Math.random() * 10);
+                    const imageUrl = data.results[random].urls.raw;
+                    document.body.style.backgroundImage = "url('" + imageUrl + "')";
+                    this.setCardBackgroundColor(imageUrl);
+                } else {
+                    console.log('No images found for', city);
+                }
+            })
+            .catch((error) => {
+                console.error('Error fetching Unsplash image:', error);
+            });
+    },
+    setCardBackgroundColor: function (imageUrl) {
+        const img = new Image();
+        img.crossOrigin = 'Anonymous'; // Needed to avoid CORS issues
+        img.src = imageUrl;
+
+        img.onload = function () {
+            const colorThief = new ColorThief();
+            const dominantColor = colorThief.getColor(img);
+            // const colorRGB = `rgb(${dominantColor.join(',')})`;
+            const alphaValue = 0.6;
+            const colorRGB = `rgba(${dominantColor.join(',')}, ${alphaValue})`;
+
+            document.querySelector('input.search-bar').style.background = `rgb(${dominantColor.join(',')})`;
+            document.querySelector('.card').style.background = colorRGB;
+            document.querySelector('.card').style.backdropFilter = 'blur(8px) saturate(150%)'; 
+            weather.setTextColorBasedOnBrightness(dominantColor);
+        };
+    },
+    setTextColorBasedOnBrightness: function (rgbColor) {
+        const brightness = (0.299 * rgbColor[0]) + (0.587 * rgbColor[1]) + (0.114 * rgbColor[2]);
+        const textColor = brightness > 146 ? 'black' : 'white';
+
+        document.querySelector('.card').style.color = textColor;
+        document.querySelector('input.search-bar').style.color = textColor;
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button")
+    .addEventListener('click', function () {
+        weather.search();
+    });
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == 'Enter') {
+        weather.search();
+    }
+});
+
 weather.fetchWeather("Zurich");
